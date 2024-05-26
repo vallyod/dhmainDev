97be40638c9712ac00bd91ea7a2ffb7e69eb162e
Kamademo <vallyod.lua@proton.me>
2024-05-26 10:13:19 -0400

    add

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 
+const expressWs = require('express-ws')(app)
+
 app.use(express.static('public'))
+
+var clients = new Set()
 
 app.get('/',  (req, res) => {
     res.sendFile('public/index.html');
 })
+
+app.ws('/socket', function(ws, req) {
+  clients.add(ws)
+
+  ws.on('message', function(msg) {
+    const message = JSON.parse(msg)
+  });
+
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+});
 
 app.listen(port, () => {
   console.log(`App open at localhost:${port}`)
