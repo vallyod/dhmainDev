a9002943ecd5048efb10fbefd0adb25a4524da00
Kamademo <vallyod.lua@proton.me>
2024-05-26 11:39:43 -0400

    server

--- server.js
+++ server.js
@@ -1,50 +1,40 @@
-const express = require('express')
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
 
-const app = express()
-const port = 3000
-
-const expressWs = require('express-ws')(app)
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static('public'))
 
-var clients = new Set()
+app.get('/', (req, res) => {
+  res.sendFile('public/FP.html');
+});
 
-app.get('/',  (req, res) => {
-    res.sendFile('public/index.html');
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-app.ws('/socket', function(ws, req) {
   // Create user
   var user = new Object();
-  user.webSocket = ws
   user.name = "placeholder"
   user.image = "image placeholder"
-  user.removing = false
 
   // Tell clients new client joined
-  for (const client of clients) {
-    client.webSocket.send(user)
-  }
-
-  // Add new client to clients
-  clients.add(user)
+  socket.emit("userJoined", user)
 
   // For recieving answers
-  ws.on('message', function(msg) {
+  socket.on('answer', function(msg) {
     const message = JSON.parse(msg)
   });
 
   // Leaving
-  ws.on('close', () => {
-    clients.delete(user);
-    user.removing = true
-
-    for (const client of clients) {
-      client.send(user)
-    }  
+  socket.on('disconnect', () => {
+    socket.emit("userDeleted", user)
   });
 });
 
-app.listen(port, () => {
-  console.log(`App open at localhost:${port}`)
-})
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
