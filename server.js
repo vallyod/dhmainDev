ca6f037cdfb32349371a7f62a73c8228eedf5791
Kamademo <vallyod.lua@proton.me>
2024-05-26 13:18:54 -0400

    g

--- server.js
+++ server.js
@@ -1,47 +1,50 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
+const { createServer } = require('http');
+const { join } = require('path');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'))
-app.use(express.static('styles'))
+app.use(express.static('public'));
+app.use(express.static('styles'));
 
 app.get('/', (req, res) => {
-  //res.sendFile('public/FP.html');
   res.sendFile(join(__dirname, 'pages', 'login.html'));
 });
 
 app.get('/game', (req, res) => {
-  //res.sendFile('public/FP.html');
   res.sendFile(join(__dirname, 'pages', 'game.html'));
 });
 
-var clients = new Set()
+const clients = new Map();
+
 io.on('connection', (socket) => {
   // Create user
-  var user = new Object();
-  //user.socket = socket
-  user.name = "placeholder"
-  user.image = "image placeholder"
+  const user = {
+    id: socket.id,
+    name: 'placeholder',
+    image: 'image placeholder'
+  };
 
-  // Tell clients new client joined
-  socket.emit("userJoined", user)
+  // Add user to clients
+  clients.set(user);
 
-  // For recieving answers
-  socket.on('answer', function(msg) {
-    const message = JSON.parse(msg)
+  clients.forEach((existingUser) => {
+    socket.emit('userJoined', existingUser);
   });
 
-  // Leaving
+  socket.broadcast.emit('userJoined', user);
+
+  // Handle disconnection
   socket.on('disconnect', () => {
-    socket.emit("userDeleted", user)
+    clients.delete(user);
+
+    io.emit('userDeleted', user);
   });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('Server running at http://localhost:3000');
 });
